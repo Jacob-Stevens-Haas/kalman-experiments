6532253184786701a0ccf3eaa31d687505a1ff61
Jake Stevens-Haas <37048747+Jacob-Stevens-Haas@users.noreply.github.com>
2022-11-01 15:11:28 -0700

    TST: Fix normal distro test for gen_data
    Also gitignore .vscode

--- tests/test_all.py
+++ tests/test_all.py
@@ -1,7 +1,16 @@
+import numpy as np
+from scipy import stats
+
 import kal_exp
 
-
 def test_gen_data():
-    _, x_true, _, _, _ = kal_exp.gen_data(1, nt=20)
-    # Should be a standard normal distribution, passing 99.8 percent of the time
-    assert abs(x_true[-2]) < 3
+    measurements, x_true, x_dot_true, H, times = kal_exp.gen_data(1, nt=20, meas_var=.1, process_var=1)
+
+    # K-S test for normality of marginal distributions of x_true and x_dot_true
+    delta_times = times[1:] - times[:-1]
+    delta_x = x_true[1:]-x_true[:-1]
+    delta_xdot = x_dot_true[1:]-x_dot_true[:-1]
+    p_x = stats.kstest(delta_x / np.sqrt(delta_times**3/3), stats.norm.cdf).pvalue
+    p_xdot = stats.kstest(delta_xdot / np.sqrt(delta_times), stats.norm.cdf).pvalue
+    assert p_x > .05
+    assert p_xdot > .05
