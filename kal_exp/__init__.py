BUG: Fix error in prior gradient

kal_exp/__init__.py
@@ -149,7 +149,7 @@ def solve_prior(
     log_add = (2 + 2 * eps) * sigma_tilde
 
     grad = prior_grad(Theta, Pi, subtract, log_coef, log_add)
-    objective = prior_obj(Pi, subtract, log_coef, log_add)
+    objective = prior_obj(Theta, Pi, subtract, log_coef, log_add)
 
     def sigma_of_x(x):
         return ((x.T @ Pi @ x + log_add) / 2 / log_coef)[0, 0]
@@ -167,10 +167,10 @@ def solve_prior(
     return x_hat, x_dot_hat, G, Qinv, sigma_hat
 
 
-def prior_obj(Pi, subtract, log_coef, log_add):
+def prior_obj(Theta, Pi, subtract, log_coef, log_add):
     def objective(x):
         return (
-            x.T @ Pi @ x
+            x.T @ Theta @ x
             - 2 * subtract.T @ x
             + log_coef * np.log(x.T @ Pi @ x + log_add)
         )
@@ -181,8 +181,8 @@ def prior_grad(Theta, Pi, subtract, log_coef, log_add):
     def grad(x):
         return (
             # H.T @ Rinv @ (H @ x - z)
-            Theta @ x
-            - subtract
+            2 * Theta @ x
+            - 2 * subtract
             # + (T+1+eps)* np.log((x.T @ G.T @ Qinv @ G @ x) + (2+2*eps)*sigma_tilde)
             + log_coef * Pi @ x / ((x.T @ Pi @ x) + log_add)
         )
@@ -244,7 +244,7 @@ def solve_marginal(
             raise ValueError("You're cheating!!!")
         else:
             raise TypeError("NAAAAAAAN")
-        objmid = objective(midpoint)
+        objmid = objective(midpoint)[0,0]
         print(f"feval: {objmid:.5} \u03C3\u00B2 estimated as {midpoint:.2}")
         if np.abs(gmid) < abs_tol:
             print("Minimizer found in iter ", i)

tests/test_all.py
@@ -113,7 +113,7 @@ def test_solve_marginal(sample_data, sigma_z, sigma_x):
 
 def test_solve_prior(sample_data, sigma_z, sigma_x):
     measurements, x_true, x_dot_true, H, times = sample_data
-    x_hat, x_dot_hat, G, Qinv = kal_exp.solve_prior(
+    x_hat, x_dot_hat, G, Qinv, sigma_hat = kal_exp.solve_prior(
         measurements, H, times, sigma_z, sigma_x
     )
     mse = np.sqrt(np.linalg.norm(x_hat - x_true) ** 2 / len(x_true))
@@ -219,7 +219,7 @@ def test_map_gradient(seed, sample_data, sigma_z, sigma_x):
     sigma_tilde = 2
     log_coef = T + 1 + eps
     log_add = (2 + 2 * eps) * sigma_tilde
-    obj = kal_exp.prior_obj(Pi, subtract, log_coef, log_add)
+    obj = kal_exp.prior_obj(Theta, Pi, subtract, log_coef, log_add)
     grad = kal_exp.prior_grad(Theta, Pi, subtract, log_coef, log_add)
     x0 = rng.normal(loc=kal_exp.restack(x_true, x_dot_true))
 