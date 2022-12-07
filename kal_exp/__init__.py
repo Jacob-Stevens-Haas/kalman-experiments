Fix bug in gradient for MAP

kal_exp/__init__.py
@@ -184,7 +184,7 @@ def prior_grad(Theta, Pi, subtract, log_coef, log_add):
             2 * Theta @ x
             - 2 * subtract
             # + (T+1+eps)* np.log((x.T @ G.T @ Qinv @ G @ x) + (2+2*eps)*sigma_tilde)
-            + log_coef * Pi @ x / ((x.T @ Pi @ x) + log_add)
+            + log_coef * 2 * Pi @ x / ((x.T @ Pi @ x) + log_add)
         )
     return grad
 

tests/test_all.py
@@ -226,10 +226,10 @@ def test_map_gradient(seed, sample_data, sigma_z, sigma_x):
     x0 = rng.normal(loc=kal_exp.restack(x_true, x_dot_true))
 
     lhs, rhs = kal_exp.gradient_test(obj, grad, x0)
-    assert np.abs(lhs/rhs-1) < 1e-3
+    assert np.abs(lhs/rhs-1) < 1e-7
 
     lhs, rhs = kal_exp.complex_step_test(obj, grad, x0)
-    assert np.abs(lhs/rhs-1) < 1e-3
+    assert np.abs(lhs/rhs-1) < 1e-7
 
 
 def root_pinv(Q, threshold=1e-15):